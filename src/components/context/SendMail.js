a2980afd9a884c659a41db9d84d84e0b100f03d4
Vignesh1725 <vigneshkumaran2315@gmail.com>
2025-09-14 17:06:54 +0530

    Updated SendMail.js

--- src/components/context/SendMail.js
+++ src/components/context/SendMail.js
@@ -4,13 +4,16 @@ import emailjs from 'emailjs-com';
 export const SendMailContext = createContext();
 
 export const SendMailProvider = ({ children })  => {
+    const service_id = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const template_id = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const public_key = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
     const sendMail = (formRef) => {
         try {
             emailjs.sendForm(
-                "service_4mkbuqg",
-                "template_tu9deuw",
+                service_id,
+                template_id,
                 formRef,
-                "HQEfvRZEyWEEZBCzg"
+                public_key
             );
             alert("Message sent successfully!");
         }
